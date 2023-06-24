6921550784bd3112450d90c9f12f19229f4a5732
Taha Sökmen <resimli1000@gmail.com>
2023-06-24 12:44:16 +0300

    Create Navbar component content

--- components/server/Navbar.jsx
+++ components/server/Navbar.jsx
@@ -1,4 +1,14 @@
+import Link from "next/link";
 import styles from "./Navbar.module.css";
 export default function Navbar() {
-  return <nav>Test</nav>;
+  return (
+    <nav className={styles.content}>
+      <nav className={styles.linkContainer}>
+        <Link href="#">About Me</Link>
+        <Link href="#">Toolset</Link>
+        <Link href="#">Contact</Link>
+        <Link href="#">CV</Link>
+      </nav>
+    </nav>
+  );
 }
