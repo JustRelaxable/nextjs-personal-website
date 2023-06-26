f471f3401259dc6a047be9ad7e8ea3fccab9f83b
Taha Sökmen <resimli1000@gmail.com>
2023-06-26 15:10:58 +0300

    Fix inter browser compability issue

--- components/client/ImageShowcase.jsx
+++ components/client/ImageShowcase.jsx
@@ -45,6 +45,7 @@ export default function ImageShowcase({
                   width: "100%",
                   height: "100%",
                   objectFit: "cover",
+                  left: `${i * 100}%`,
                 }}
                 placeholder="blur"
               ></Image>
