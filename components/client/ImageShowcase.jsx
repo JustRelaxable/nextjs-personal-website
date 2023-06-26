05b0daab55226ceaa46bc9fade9e11bf4ddd738c
Taha Sökmen <resimli1000@gmail.com>
2023-06-26 20:01:43 +0300

    Add ImageShowcase component svg icons

--- components/client/ImageShowcase.jsx
+++ components/client/ImageShowcase.jsx
@@ -26,11 +26,29 @@ export default function ImageShowcase({
         <button
           className={`${styles.arrow} ${styles.left}`}
           onClick={previousImage}
-        ></button>
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 320 512"
+            className={styles.icon}
+          >
+            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+          </svg>
+        </button>
         <button
           className={`${styles.arrow} ${styles.right}`}
           onClick={nextImage}
-        ></button>
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 320 512"
+            className={styles.icon}
+          >
+            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+          </svg>
+        </button>
         <div
           className={styles.imageContainer}
           style={{ translate: `${-imageIndex * 100}%` }}
