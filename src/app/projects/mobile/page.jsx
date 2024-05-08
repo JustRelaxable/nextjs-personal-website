44d702cfedb2911d30d339f5fc634e4b7dd8f002
Taha Sökmen <resimli1000@gmail.com>
2024-05-08 15:30:55 +0300

    add TurkTvBox to mobile app projects

--- src/app/projects/mobile/page.jsx
+++ src/app/projects/mobile/page.jsx
@@ -5,6 +5,12 @@ import VerticalSeperator from "../../../../components/server/layout/VerticalSepe
 
 import auto1 from "../../../../public/project-screenshots/mobile/autotrader19/autotrader19-1.jpg";
 
+import turktvbox1 from "../../../../public/project-screenshots/mobile/turktvbox/turktvbox-1.jpg";
+import turktvbox2 from "../../../../public/project-screenshots/mobile/turktvbox/turktvbox-2.jpg";
+import turktvbox3 from "../../../../public/project-screenshots/mobile/turktvbox/turktvbox-3.jpg";
+import turktvbox4 from "../../../../public/project-screenshots/mobile/turktvbox/turktvbox-4.jpg";
+import turktvbox5 from "../../../../public/project-screenshots/mobile/turktvbox/turktvbox-5.jpg";
+
 export default function MobileProjectsPage() {
   return (
     <section>
@@ -36,6 +42,36 @@ export default function MobileProjectsPage() {
           have added and the automated trading strategies. With this
           application, you can monitor the market conditions, adjust trading
           strategies, and modify your pairs when necessary.
+        </MobileProject>
+        <VerticalSeperator></VerticalSeperator>
+        <MobileProject
+          headerText={"TurkTVBox"}
+          imageData={[
+            {
+              path: turktvbox1,
+              caption: "Main page with continue from where you left",
+            },
+            {
+              path: turktvbox2,
+              caption: "Anime category",
+            },
+            {
+              path: turktvbox3,
+              caption: "Series episode selection & details",
+            },
+            {
+              path: turktvbox4,
+              caption: "Season selection & episode selection",
+            },
+            {
+              path: turktvbox5,
+              caption: "Video player",
+            },
+          ]}
+        >
+          TurkTvBox is a video streaming app with the functionality to save
+          where you left off. It scrapes the data from configurable sources. It
+          is coded with kotlin and UI with Jetpack Compose.
         </MobileProject>
       </PagePadding>
     </section>
